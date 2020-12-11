41b49d4e22f82507bdd4a8773a945b3108b66430
zed <zed@kolk.me>
2020-12-11 23:09:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,10 +12,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num == 1
+  if num == "1"
     binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num == 2
+  elsif num = "2"
     binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
