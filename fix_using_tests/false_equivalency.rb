acbcd16b2a0131346a41a305fbe4667133c99ccc
zed <zed@kolk.me>
2020-12-11 23:09:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -15,7 +15,7 @@ def selection(num)
   if num == "1"
     binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = "2"
+  elsif num == "2"
     binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
