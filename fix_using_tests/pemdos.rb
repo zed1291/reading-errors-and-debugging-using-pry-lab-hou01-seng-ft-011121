d66eddd918532fdbc9ac4fdb1a11472d7c38ef69
zed <zed@kolk.me>
2020-12-11 23:00:08 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,6 +2,14 @@
 
 def snake_it_up(string)
   if string[0] == "s"
+    string = 10 * "s" + string
+  else
+    string
+  end
+end
+
+def snake_it_up(string)
+  if string[0] == "s"
     string = "ssssssssss" + string
   else
     string
